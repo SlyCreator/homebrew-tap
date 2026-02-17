3314a6b57663e616ae9dfd4e0b7a4b8845dbcf2f
Slycreator <chinonso1amaechi@gmail.com>
2026-02-17 10:29:50 +0100

    Drop Intel support, Apple Silicon only for v1.0.2

--- Casks/speakclaw.rb
+++ Casks/speakclaw.rb
@@ -1,15 +1,10 @@
 cask "speakclaw" do
   version "1.0.2"
 
-  on_arm do
-    url "https://speakclaw-releases.s3.eu-west-1.amazonaws.com/desktop/v#{version}/SpeakClaw_#{version}_aarch64.dmg"
-    sha256 "eab983eddeb74eba91785bb9c80f97eaf03b96bf30947a488a55132333589c05"
-  end
+  url "https://speakclaw-releases.s3.eu-west-1.amazonaws.com/desktop/v#{version}/SpeakClaw_#{version}_aarch64.dmg"
+  sha256 "eab983eddeb74eba91785bb9c80f97eaf03b96bf30947a488a55132333589c05"
 
-  on_intel do
-    url "https://speakclaw-releases.s3.eu-west-1.amazonaws.com/desktop/v#{version}/SpeakClaw_#{version}_x64.dmg"
-    sha256 "9995218ac914c1f593c2fc31481c37ac7e08a9410978d82d7d393ad0fd481884"
-  end
+  depends_on arch: :arm64
 
   name "SpeakClaw"
   desc "System-wide voice-to-text for macOS"
